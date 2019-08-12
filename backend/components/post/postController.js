eef5d0a621bdfb08ec964ec3dd6d99003f396ff0
nireo <eemil.lehtikangas1@gmail.com>
2019-08-12 21:01:49 +0300

    token stuff

--- backend/components/post/postController.js
+++ backend/components/post/postController.js
@@ -1,26 +1,40 @@
+const jwt = require('jsonwebtoken');
 const postModel = require('./postModel');
+const { getToken } = require('../../utils/helper');
+const config = require('../../utils/config');
 
 exports.getAllPosts = async (req, res, next) => {
   try {
-    await postModel.find({}).exec((err, results) => {
-      if (err) return res.status(500);
+    await postModel
+      .find({})
+      .populate('postedBy')
+      .exec((err, results) => {
+        if (err) return res.status(500);
 
-      return res.json(results);
-    });
+        return res.json(results);
+      });
   } catch (e) {
     next(e);
   }
 };
 
 exports.createPost = async (req, res, next) => {
+  const token = getToken(req);
   try {
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!token || !decodedToken) {
+      return res.status(401).json({
+        error: 'invalid token'
+      });
+    }
     const { title, description, price } = req.body;
     const time = new Date();
     const newPost = new postModel({
       title,
       description,
       price,
-      posted: time
+      posted: time,
+      postedBy: decodedToken.id
     });
 
     const savedPost = await newPost.save();
--- backend/components/review/reviewController.js
+++ backend/components/review/reviewController.js
@@ -1,16 +1,32 @@
+const jwt = require('jsonwebtoken');
 const reviewModel = require('./reviewModel');
+const userModel = require('../user/userModel');
+const config = require('../../utils/config');
+const { getToken } = require('../../utils/helper');
 
 exports.createReview = async (req, res, next) => {
+  const { stars, title, description, recommended } = req.body;
+  const token = getToken(req);
   try {
-    const { stars, title, description, recommended } = req.body;
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!token || !decodedToken) {
+      return res.status(401).json({
+        error: 'invalid token'
+      });
+    }
+
+    const user = await userModel.findById(decodedToken.id);
+
     const newReview = new reviewModel({
       title,
       recommended,
       stars,
-      description
+      description,
+      postedBy: user.id
     });
-
     const saved = await newReview.save();
+    user.reviewsPosted = user.reviewsPosted.concat(saved._id);
+    await user.save();
     return res.json(saved);
   } catch (e) {
     next(e);
