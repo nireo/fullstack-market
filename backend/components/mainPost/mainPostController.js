df0683f1d0f3529f42d1ac16b53012766a1df467
nireo <eemil.lehtikangas1@gmail.com>
2019-08-13 09:15:18 +0300

    added mainPost route

--- backend/components/mainPost/mainPostController.js
+++ backend/components/mainPost/mainPostController.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const mainPostModel = require('./mainPostModel');
+const { getToken } = require('../../utils/helper');
+const config = require('../../utils/config');
 
 exports.getAllPosts = async (req, res, next) => {
   try {
@@ -12,4 +15,30 @@ exports.getAllPosts = async (req, res, next) => {
   }
 };
 
-exports.createPost = async (req, res, next) => {};
+exports.createPost = async (req, res, next) => {
+  const { title, description, price } = req.body;
+  const token = getToken(req);
+  try {
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!decodedToken || !token) {
+      return res.status(401).json({
+        error: 'invalid token'
+      });
+    }
+    if (decodedToken.username === 'admin') {
+      const newMainPost = new mainPostModel({
+        title,
+        description,
+        price
+      });
+
+      const saved = await newMainPost.save();
+      return res.json(saved);
+    }
+    return res.status(403).json({
+      error: 'unauthorized'
+    });
+  } catch (e) {
+    next(e);
+  }
+};
