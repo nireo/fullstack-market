095b2f50328264d05b9a91afbb51e9a8e00911c4
nireo <eemil.lehtikangas1@gmail.com>
2019-10-14 02:10:14 +0300

    added more features to report service

--- backend/components/report/reportController.js
+++ backend/components/report/reportController.js
@@ -15,7 +15,7 @@ exports.createReport = async (req, res, next) => {
       });
     }
     const from = await userModel.findById(decodedToken.id);
-    const to = await userModel.findById(request.params.id);
+    const to = await userModel.findById(req.params.id);
     const newReport = new reportModel({
       content,
       to: to._id,
@@ -23,6 +23,54 @@ exports.createReport = async (req, res, next) => {
     });
     const saved = await newReport.save();
     return res.json(saved);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.getReports = async (req, res, next) => {
+  const token = getToken(req);
+  try {
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!decodedToken || !token) {
+      return res.status(401).json({
+        error: 'invalid token'
+      });
+    }
+    const user = await userModel.findById(decodedToken._id);
+    // reports only for admins, since there is no need for others to see reports
+    if (user.username === 'admin') {
+      const reports = await reportModel.find();
+      return res.json(reports);
+    } else {
+      return res.status(403).json({
+        error: 'unauthorized'
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.deleteReports = async (req, res, next) => {
+  const token = getToken(req);
+  try {
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!decodedToken || !token) {
+      return res.status(401).json({
+        error: 'invalid token'
+      });
+    }
+    const user = await userModel.findById(decodedToken._id);
+    const report = await reportModel.findById(req.params.id);
+    if (user.username === 'admin' || user._id === report.from) {
+      await userModel.findByIdAndRemove(req.params.id);
+      return res.status(204).end();
+    } else {
+      return res.status(403).json({
+        error: 'invalid token'
+      });
+    }
   } catch (e) {
     next(e);
   }
--- backend/components/report/reportRouter.js
+++ backend/components/report/reportRouter.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const reportController = require('./reportController');
 
 router.post('/:id', reportController.createReport);
+router.get('', reportController.getReports);
+router.delete('/:id', reportController.deleteReports);
 
 module.exports = router;
