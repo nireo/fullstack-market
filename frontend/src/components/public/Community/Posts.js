04b92bec25642e0af661eeb9374031639691be1d
nireo <eemil.lehtikangas1@gmail.com>
2019-10-17 00:35:10 +0300

    removed amount in page displayers

--- frontend/src/components/public/Community/Posts.js
+++ frontend/src/components/public/Community/Posts.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Pagination from '../Pagination';
-import { initPosts } from '../../../reducers/postReducer';
-import { setNotification } from '../../../reducers/notificationReducer';
-import { addItemToCart } from '../../../reducers/cartReducer';
-import Loading from '../../Loading';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Pagination from "../Pagination";
+import { initPosts } from "../../../reducers/postReducer";
+import { setNotification } from "../../../reducers/notificationReducer";
+import { addItemToCart } from "../../../reducers/cartReducer";
+import Loading from "../../Loading";
 
 const Posts = props => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [amountInPage, setAmountInPage] = useState(3);
+  const [amountInPage] = useState(3);
   useEffect(() => {
     if (props.posts === null) {
       props.initPosts();
@@ -26,10 +26,10 @@ const Posts = props => {
       checkForItem = props.cart.find(i => i._id === toAdd._id);
     }
     if (checkForItem) {
-      props.setNotification('Already in cart', 'error', 2);
+      props.setNotification("Already in cart", "error", 2);
     }
     props.addItemToCart(toAdd);
-    props.setNotification('Item added to cart', 'success', 2);
+    props.setNotification("Item added to cart", "success", 2);
   };
 
   const lastPostIndex = currentPage * amountInPage;
@@ -39,13 +39,13 @@ const Posts = props => {
 
   const renderPosts = currentPosts.map(p => (
     <div key={p._id} className="col-md 4">
-      <div className="card" style={{ marginTop: '1em' }}>
+      <div className="card" style={{ marginTop: "1em" }}>
         <div className="card-body">
           <h5 className="card-title">{p.title}</h5>
-          <h6 className="card-subtitle" style={{ color: 'green' }}>
+          <h6 className="card-subtitle" style={{ color: "green" }}>
             {p.price} $
           </h6>
-          <p className="card-text" style={{ paddingBottom: '1rem' }}>
+          <p className="card-text" style={{ paddingBottom: "1rem" }}>
             {p.description.slice(0, 250)}
           </p>
           {props.user && (
@@ -62,21 +62,7 @@ const Posts = props => {
   ));
 
   return (
-    <div class="container" style={{ paddingTop: '1rem' }}>
-      <div className="row">
-        <div className="col-md-10">
-          <p>Posts per page: {amountInPage}</p>
-        </div>
-        <div className="col-md-2">
-          Select posts per page
-          <select className="form-control form-control-sm">
-            <option onClick={() => setAmountInPage(2)}>2</option>
-            <option onClick={() => setAmountInPage(3)}>3</option>
-            <option onClick={() => setAmountInPage(4)}>4</option>
-            <option onClick={() => setAmountInPage(5)}>5</option>
-          </select>
-        </div>
-      </div>
+    <div class="container" style={{ paddingTop: "1rem" }}>
       <hr />
       {renderPosts.length === 0 ? (
         <div className="text-center">
@@ -86,7 +72,7 @@ const Posts = props => {
         renderPosts
       )}
       <hr />
-      <div class="container" style={{ paddingTop: '1rem' }}>
+      <div class="container" style={{ paddingTop: "1rem" }}>
         <Pagination
           amountInPage={amountInPage}
           totalPosts={props.posts.length}
--- frontend/src/components/public/MainPost/MainPosts.js
+++ frontend/src/components/public/MainPost/MainPosts.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { initMainPosts } from '../../../reducers/mainReducer';
-import '../../../utils/loadingBar.css';
-import Loading from '../../Loading';
-import { Link } from 'react-router-dom';
-import { addItemToCart } from '../../../reducers/cartReducer';
-import { setNotification } from '../../../reducers/notificationReducer';
-import Pagination from '../Pagination';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { initMainPosts } from "../../../reducers/mainReducer";
+import "../../../utils/loadingBar.css";
+import Loading from "../../Loading";
+import { Link } from "react-router-dom";
+import { addItemToCart } from "../../../reducers/cartReducer";
+import { setNotification } from "../../../reducers/notificationReducer";
+import Pagination from "../Pagination";
 
 const MainPosts = props => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [amountInPage, setAmountInPage] = useState(3);
+  const [amountInPage] = useState(3);
   useEffect(() => {
     if (props.mainPost === null) {
       props.initMainPosts();
@@ -27,10 +27,10 @@ const MainPosts = props => {
       checkForItem = props.cart.find(i => i._id === toAdd._id);
     }
     if (checkForItem) {
-      props.setNotification('Already in cart', 'error', 2);
+      props.setNotification("Already in cart", "error", 2);
     }
     props.addItemToCart(toAdd);
-    props.setNotification('Item added to cart', 'success', 2);
+    props.setNotification("Item added to cart", "success", 2);
   };
 
   const lastPostIndex = currentPage * amountInPage;
@@ -40,13 +40,13 @@ const MainPosts = props => {
 
   const renderPosts = currentPosts.map(p => (
     <div key={p._id} className="col-md 4">
-      <div className="card" style={{ marginTop: '1em' }}>
+      <div className="card" style={{ marginTop: "1em" }}>
         <div className="card-body">
           <h5 className="card-title">{p.title}</h5>
-          <h6 className="card-subtitle" style={{ color: 'green' }}>
+          <h6 className="card-subtitle" style={{ color: "green" }}>
             {p.price} $
           </h6>
-          <p className="card-text" style={{ paddingBottom: '1rem' }}>
+          <p className="card-text" style={{ paddingBottom: "1rem" }}>
             {p.description.slice(0, 250)}
           </p>
           {props.user && (
@@ -64,20 +64,7 @@ const MainPosts = props => {
   ));
 
   return (
-    <div className="container" style={{ paddingTop: '1rem' }}>
-      <div className="row">
-        <div className="col-md-10">
-          <p>Posts per page: {amountInPage}</p>
-        </div>
-        <div className="col-md-2">
-          Select posts per page
-          <select className="form-control form-control-sm">
-            <option onClick={() => setAmountInPage(3)}>3</option>
-            <option onClick={() => setAmountInPage(4)}>4</option>
-            <option onClick={() => setAmountInPage(5)}>5</option>
-          </select>
-        </div>
-      </div>
+    <div className="container" style={{ paddingTop: "1rem" }}>
       <hr />
       {renderPosts.length === 0 ? (
         <div className="text-center">
@@ -87,7 +74,7 @@ const MainPosts = props => {
         renderPosts
       )}
       <hr />
-      <div className="container" style={{ paddingTop: '1rem' }}>
+      <div className="container" style={{ paddingTop: "1rem" }}>
         <Pagination
           amountInPage={amountInPage}
           totalPosts={props.mainPost.length}
