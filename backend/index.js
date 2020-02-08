f07bd25d55f530377e7e69e136106b5a1a614181
nireo <eemil.lehtikangas1@gmail.com>
2020-02-09 01:00:15 +0200

    removed chat from backend

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,7 @@
 const app = require('./app');
 const server = require('http').createServer(app);
 const config = require('./utils/config');
-const io = (module.exports.io = require('socket.io')(server));
-const socketManager = require('./utils/socket-manager');
 const logger = require('./utils/logger');
-const {
-  addUser,
-  removeUser,
-  getUser,
-  getUsersInChat
-} = require('./socket/users');
-const user = {
-  username: 'Benelov Bot',
-  id: '1'
-};
-
-io.on('connection', socket => {
-  socket.on('join', data => {
-    addUser(socket.id, data.id, data.username);
-    socket.emit('message', {
-      user,
-      text: 'Welcome to the chat room.'
-    });
-    socket.broadcast.emit('chatData', { users: getUsersInChat });
-  });
-
-  socket.on('messageSent', message => {
-    const user = getUser(socket.id);
-    io.emit('message', { user: user, text: message });
-  });
-
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
-    if (user) {
-      io.emit('message', {
-        user,
-        text: `${user.username} has left.`
-      });
-
-      io.emit('chatData', { users: getUsersInChat });
-    }
-  });
-});
 
 server.listen(config.PORT, () => {
   logger.info(`[express] Server running on port ${config.PORT}`);
